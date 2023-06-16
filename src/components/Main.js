aa3f73019e791b55ef92f517c496a0ff0c75e682
Seth Powelson <powelson@mgstover.com>
2023-06-16 11:52:03 -0600

    img template

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,30 @@
 import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ChatClient } from '../renderer/chat-client';
+import { Card } from '@mui/material';
+import { CardMedia } from '@mui/material';
 import './Main.css'
 
 
 export default function Main() {
+  const imageString = 'data:image/png;base64,iVBORw0KG...'; // Your base64 string here
+
   return (
     <div className='grid-parent'>
       <div className='grid-left'>
         <h1 className='grid-title'>Meet <b className='ottogpt'>OttoGPT</b></h1>
-        <ul>
-          <li>Understands plain English</li>
-          <li>Results in seconds</li>
-          <li>Available 24/7</li>
+        <ul className='grid-list'>
+          <li>✅ Understands plain English</li>
+          <li>✅ Results in seconds</li>
+          <li>✅ Available 24/7</li>
         </ul>
-        <br/>
-        <h5>Try it now!</h5>
+        <Card className='message-image'>
+          <CardMedia
+            component="img"
+            height="140"
+            image={imageString}
+            alt="User Image"
+          />
+        </Card>
       </div>
       <div className='grid-right'>
         <ChatClient />
